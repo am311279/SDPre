3982e92c24a7153b29e7b45651ccef000c6cbd20
Aung Myat <aungmyat1@gmail.com>
2025-04-27 22:54:25 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+	public static void main (String[] args) {
+		String name = "Aung Myat";
+		int age = 45;
+		String state = "Yangon";
+
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state );
+	}
+}
+

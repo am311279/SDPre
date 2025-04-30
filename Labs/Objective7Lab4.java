bf317a25234569e5344908969ff7ca8d6871ba8c
Aung Myat <aungmyat1@gmail.com>
2025-04-29 21:20:08 -0600

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int currentSum = 0;
+		int count = 0;
+
+		while(count < 20)
+		{
+		  count = count + 1;
+		  currentSum = currentSum + count;
+		}
+
+		System.out.println(currentSum);
+	}
+}

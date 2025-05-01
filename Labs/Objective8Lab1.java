382eade8c03bfd3901237202de3da5a9cbcb13dc
Aung Myat <aungmyat1@gmail.com>
2025-04-30 18:16:02 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,15 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		//for loop that will print the number 1 to 10
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+
+		//for loop that will print the numbers 10 to 1
+		for(int j=10; j>=1; j--)
+		{
+			System.out.println(j);
+		}
+	}
+}

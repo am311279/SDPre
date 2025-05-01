9e316f2052c61d55bbeee0e17886a82221fbe70e
Aung Myat <aungmyat1@gmail.com>
2025-04-30 18:26:45 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,12 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int currentSumTotal = 0;
+
+		for(int loop = 1; loop<=20; loop++)
+		{
+			currentSumTotal += loop;
+		}
+
+		System.out.println(currentSumTotal);
+	}
+}
